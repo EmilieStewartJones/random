48ee486617010ce09f543e5cbdc8783bd243f73d
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-07-16 16:07:15 -0400

    Minor formatting changes

--- f.Spatialdf.R
+++ f.Spatialdf.R
@@ -1,14 +1,20 @@
-# Create spatial polygons dataframe of ice islands from queried table
-
+################ Function that creates spatial polygons dataframe from queried table ######################
+## Input: - Dataframe with inst, motherinst and geom1 columns  
+## Output: - A SpatialPolygonsDataFrame of ice islands
+###########################################################################################################
 
 f.Spatialdf <- function(df) {
-  row.names(df) -> df$gid
-  # Transforming polygons into SpatialPolygonsDataFrame
-  df_sp <- lapply(seq(nrow(df)), FUN=function(x) readWKT(df$geom1[x], df$gid[x]))
-  df_sp <- do.call(rbind, df_sp)
-  df_sp <- SpatialPolygonsDataFrame(df_sp, df)
+  # Add a column with an id number
+    row.names(df) -> df$gid
+
+  # Transforming polygons in to SpatialPolygonsDataFrame
+    df_sp <- lapply(seq(nrow(df)), FUN=function(x) readWKT(df$geom1[x], df$gid[x]))
+    df_sp <- do.call(rbind, df_sp)
+    df_sp <- SpatialPolygonsDataFrame(df_sp, df)
+    
   # assining original proj (LCC)
-  proj4string(df_sp) = CRS("+proj=lcc +lat_1=77 +lat_2=49 +lat_0=40 +lon_0=-100, ellps=WGS84")
+    proj4string(df_sp) = CRS("+proj=lcc +lat_1=77 +lat_2=49 +lat_0=40 +lon_0=-100, ellps=WGS84")
+  
   return(df_sp)
 }
 
