198059ea81c0d8dcb7e3c4ec0fa6e0d461729b27
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-07-16 15:10:13 -0400

    Minor formatting changes

--- f.coast.R
+++ f.coast.R
@@ -1,16 +1,24 @@
-# Function to bring in and prepare coastline data
-# Can only be used with gshhs_l_l1 dataframe
+############### Function to bring in and prepare coastline data #######################################
+#### Can only be used with gshhs_l_l1 dataframe
+## Input: - Connection to database
+##        - Map extent as a list of coordinates, in this order: min x, max x, min y, max y (optional)
+## Output: - An object of class SpatialPolygons of the cropped coastal map
+#######################################################################################################
+# TO DO: - make compatable with any of the gshhs maps
 
-f.coast <- function(con, map_extent = NULL) {
+f.coast <- function(conn, map_extent = NULL) {
+  library(sp)
+  
   # Define functions
     gClip <- function(shp, bb){
       if(class(bb) == "matrix") b_poly <- as(extent(as.vector(t(bb))), "SpatialPolygons")
       else b_poly <- as(extent(bb), "SpatialPolygons")
       gIntersection(shp, b_poly, byid = TRUE)
     }
+    
   # Get from db
     query <- "SELECT ST_AsText(gshhs_l_l1.wkb_geometry) AS geom FROM gshhs_l_l1"
-    coast <- dbGetQuery(con, query)
+    coast <- dbGetQuery(conn, query)
     row.names(coast) -> coast$gid
   
   # Make into SpatialPolygonDataFrame
@@ -21,8 +29,10 @@ f.coast <- function(con, map_extent = NULL) {
     # reassign LCC
     coast <- spTransform(coast, CRS("+proj=lcc +lat_1=77 +lat_2=49 +lat_0=40 +lon_0=-100, ellps=WGS84"))
   
-  # clip to same extent as site map
+  # crop map
     if (is.null(map_extent)) crop <- extent(0, 2000000, 2800000, 5000000)
     else crop <- extent(map_extent[1], map_extent[2], map_extent[3], map_extent[4])
     coastcrop <- gClip(coast, crop)
+    
+    return(coastcrop)
 }
