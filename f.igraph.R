d52a18587479f13169da6bcaf3295520faef4754
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-07-16 16:10:37 -0400

    Minor formatting changes

--- f.igraph.R
+++ f.igraph.R
@@ -1,23 +1,26 @@
 ######################## Function that creates an igraph object ##############################
-##### Input: 'table' with motherinst, inst and any other attributes ####
+##### Input:  Dataframe with at the least the motherinst and inst columns
 ##### Output: igraph object that includes all attributes ####
 ##############################################################################################
 
 f.igraph <- function(table){
   library(igraph)
-  edgelist <- data.frame(table$motherinst, table$inst) 
-  colnames(edgelist) <- c("motherinst", "inst")
-  edgelist <- as.data.frame(edgelist[edgelist$motherinst %in% edgelist$inst,])
+  
+  # Create edgelist 
+    edgelist <- data.frame(table$motherinst, table$inst) 
+    colnames(edgelist) <- c("motherinst", "inst")
+    edgelist <- as.data.frame(edgelist[edgelist$motherinst %in% edgelist$inst,])
   
   # Creating attribute table for vertices
-  undescribedmothers = na.omit(data.frame(unique(table$motherinst[!(table$motherinst %in% table$inst)]),
+    undescribedmothers = na.omit(data.frame(unique(table$motherinst[!(table$motherinst %in% table$inst)]),
                                           stringsAsFactors = F))  # these are the mothers that are referred to in the edgelist but have no inst
-  vattrib_df = cbind(table$inst, table[,names(table)!='inst'], stringsAsFactors =F)  # make a dataframe that starts with the vertex name and matches the inst in edgelist
-  names(vattrib_df)[1] = 'inst'
-  names(undescribedmothers)[1] = 'inst'
-  vattrib_df = rbind.fill(vattrib_df,undescribedmothers)  # Now the undescribed mothers are in the vertices attribute table without any attributes (because they don't have an inst)
+    vattrib_df = cbind(table$inst, table[,names(table)!='inst'], stringsAsFactors =F)  # make a dataframe that starts with the vertex name and matches the inst in edgelist
+    names(vattrib_df)[1] = 'inst'
+    names(undescribedmothers)[1] = 'inst'
+    vattrib_df = rbind.fill(vattrib_df,undescribedmothers)  # Now the undescribed mothers are in the vertices attribute table without any attributes (because they don't have an inst)
   
   # Creating object of class igraph
-  g <- graph_from_data_frame(edgelist, vertices = vattrib_df) #igraph function to make a graph using edges and assign attribs to vertices
+    g <- graph_from_data_frame(edgelist, vertices = vattrib_df) #igraph function to make a graph using edges and assign attribs to vertices
+  
   return(g)
 }
