eed19a8a5bd1d6fe132d24962b534f077a24db9c
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-07-16 16:44:00 -0400

    Minor formatting changes

--- f.plot2.R
+++ f.plot2.R
@@ -1,28 +1,34 @@
 ######################################### Plotting movement of ice islands ################################ 
-# Takes a spatially referenced igraph object and a spatial polygons dataframe that contains all the polygons of the igraph
+## Input: - Connection to database
+##        - A spatially referenced igraph object 
+##        - A spatial polygons dataframe that contains all the polygons of the igraph.
+##               - It may contain other polygons too, but these will not appear in the plot
+## Output: - A plot of ice islands and their edges with the coastline
+###########################################################################################################
+### TO DO: Write seperate function that creates shapefiles
 
 f.plot2 <- function(con, g, df_sp) {
   source('f.coast.R')
   library(raster)
   library(sp)
   
-  
-  df_sp <- df_sp[which(df_sp@data$inst %in% V(g)$name),]
+  # Subset the spatial polygons dataframe so that it only includes the islands of g
+    df_sp <- df_sp[which(df_sp@data$inst %in% V(g)$name),]
   
   # Prepare extent for coastline stuff
-  map_extent <- c(min(df_sp@data$centroid_x) - 20000, max(df_sp@data$centroid_x) + 20000,
+    map_extent <- c(min(df_sp@data$centroid_x) - 20000, max(df_sp@data$centroid_x) + 20000,
                   min(df_sp@data$centroid_y) - 20000, max(df_sp@data$centroid_y) + 20000)
   
   # Bring in coastline
-  coast <- f.coast(con, map_extent)
+    coast <- f.coast(con, map_extent)
   
-  ### PLOT ##########
-  par(mar=c(1,1,1,1))
-  plot(coast)
-  plot(g, vertex.shape="none", vertex.label="",
-       edge.arrow.width=0.01, edge.arrow.size = 0.01, rescale=FALSE, add=TRUE)
-  plot(df_sp, add=TRUE, col = "blue")
-  plot <- plot(coast, add=TRUE, col="beige")
+  # Plot
+    par(mar=c(1,1,1,1))
+    plot(coast)  # Plot coastline
+    plot(g, vertex.shape="none", vertex.label="",
+       edge.arrow.width=0.01, edge.arrow.size = 0.01, rescale=FALSE, add=TRUE)  # Plot edges
+    plot(df_sp, add=TRUE, col = "blue")  # Plot the polygons of the ice islands
+    plot <- plot(coast, add=TRUE, col="beige")   
   
   return(plot)
 }
