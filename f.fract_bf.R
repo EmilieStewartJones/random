38820da7507bd9ef851b39a6dd59a08e908faebe
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-08-15 15:02:42 -0500

    Outputs table of all instances just before fracturing

---
+++ f.fract_bf.R
@@ -0,0 +1,19 @@
+##### Just before fracturing #####
+f.fract_bf <- function(con, calvingyr = NULL, calvingloc = NULL) {
+  require(RPostgreSQL)
+  source('f.subquery.R')
+  
+  # Form subquery
+  sub_query <- f.subquery(calvingyr, calvingloc)
+  
+  # Query db for all instances just before fracturing  
+  query <- paste0("SELECT *, ST_AsText(shapefiles_qc_2013.wkb_geometry) as geom1,", 
+                  " DENSE_RANK() OVER(PARTITION BY calvingyr ORDER BY date_trunc('week', shapefiles_qc_2013.scenedate::timestamp))", 
+                  " as wk_num FROM shapefiles_qc_2013",
+                  " WHERE inst IN (SELECT motherinst FROM shapefiles_qc_2013 WHERE ",
+                  sub_query, " GROUP BY motherinst HAVING COUNT(*) > 1)")
+  
+  fract_bf <- dbGetQuery(con, query)
+  
+  return(fract_bf)
+}
