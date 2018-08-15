56d26b6114a13377f008d865a66482bb1e5157bb
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-08-15 15:15:27 -0500

    Now uses f.subquery

--- f.origins_db.R
+++ f.origins_db.R
@@ -5,28 +5,15 @@
 ################################################################################################################################
 # Includes orphans and instances with motherinst like _P08, _P12, _P13, _S12...
 
-###### From database####
+###### From database ######
 f.origins_db <- function(con, calvingyr = NULL, calvingloc = NULL) {
   require(RPostgreSQL)
+  source('f.subquery.R')
   
   # Prepare DB query
-    # Prepare calvingyr query
-      if (is.null(calvingyr)) {
-        yr_query <- NULL
-      }
-      else yr_query <- paste0("AND calvingyr = ANY (SELECT calvingyr WHERE calvingyr = '",
-                              paste0(calvingyr, collapse = "' OR calvingyr = '"), "')")
-      
-    # Prepare calvingloc query
-      if (is.null(calvingloc)) {
-        loc_query <- NULL
-      }
-      else loc_query <- paste0("AND calvingloc = ANY (SELECT calvingloc WHERE calvingloc = '",
-                               paste0(calvingloc, collapse = "' OR calvingloc = '"), "')")
-      
-    # Prepare total subquery
-      sub_query <- paste0(yr_query, loc_query)
-    
+    # Form subquery
+      sub_query <- f.subquery(calvingyr, calvingloc)
+
     # Prepare full query
       query <- paste0("SELECT inst, motherinst FROM shapefiles_qc_2013 WHERE motherinst", 
                       " SIMILAR TO '%(P|S)(0|1|2)(0|1|2|3|4|5|6|7|8|9)' ",
