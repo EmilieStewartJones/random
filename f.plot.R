30b2f3aa40fe4468d21c1ac9d5a83d91591625e1
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-08-22 12:44:09 -0500

    Now has option of plotting without the coastline
    Default is plotting without the coasline

--- f.plot.R
+++ f.plot.R
@@ -1,37 +1,49 @@
 ######################################### Plotting movement of ice islands ################################ 
-## Input: - Connection to database
-##        - Dataframe with at the least the inst, motherinst and geom1 columns
-## Output: - A plot of ice islands and their edges with the coastline
+## Input: - con: connection to database
+##        - df: dataframe with at the least the inst, motherinst and geom1 columns
+##        - coastline: if TRUE, ice islands will be plotted with coastline (default FALSE)
+## Output: - A plot of ice islands and their edges with or without the coastline
 ###########################################################################################################
-### Write seperate function that creates shapefiles
+### Could Write seperate function that creates shapefiles
 
-f.plot <- function(con, df) {
+f.plot <- function(con, df, coastline = FALSE, coastm = "fine") {
   source('f.coast.R')
   source('f.igraph_s.R')
   library(raster)
   library(sp)
   
   # Create spatially referenced igraph object
+    print("Creating Igraph object from dataframe")
     g <- f.igraph_s(df)
   
-  # Prepare map_extent for f.coast function
-    map_extent <- c(min(df_sp@data$centroid_x) - 20000, max(df_sp@data$centroid_x) + 20000,
-                  min(df_sp@data$centroid_y) - 20000, max(df_sp@data$centroid_y) + 20000)
-  
   # Bring in cropped coastline
-    coast <- f.coast(con, map_extent)
-  
+    if (coastline == TRUE) {
+      print("Preparing coastline data")
+      # Prepare map_extent for f.coast function
+        map_extent <- c(min(df_sp@data$centroid_x) - 25000, max(df_sp@data$centroid_x) + 25000,
+                        min(df_sp@data$centroid_y) - 25000, max(df_sp@data$centroid_y) + 25000)
+        coast <- f.coast(con, map_extent, coastm)
+      # Plot
+        print("Plotting")
+        par(mar=c(1,1,1,1))
+        plot(coast) # plot coastline
+        plot(g, vertex.shape="none", vertex.label="",
+             edge.arrow.width=0.4, edge.arrow.size = 0.3, rescale=FALSE, add=TRUE)#, vertex.label = V(g)$inst, vertex.label.cex = 0.5, vertex.label.color = 'black') # plot igraph
+        plot(df_sp, add=TRUE, col = "blue", border= "black") # Plot ice island polygons 
+        plot <- plot(coast, add=TRUE, col="beige")
+    }
+    else if (coastline == FALSE) {
+      # Plot
+        print("Plotting")
+        par(mar=c(1,1,1,1))
+        plot(df_sp, col = "blue", border= "black")
+        plot <- plot(g, add=TRUE, vertex.shape="none", vertex.label="",
+                edge.arrow.width=0.4, edge.arrow.size = 0.3, rescale=FALSE)#, vertex.label = V(g)$inst, vertex.label.cex = 0.5, vertex.label.color = 'black') # plot igraph
+    }
+
   # Create shapefiles
     #shapefile(coast, "coast.shp", overwrite = TRUE)
     #shapefile(df_sp, "polygons.shp", overwrite = TRUE)
-  
-  # plot
-    par(mar=c(1,1,1,1))
-    plot(coast) # plot coastline
-    plot(g, vertex.shape="none", vertex.label="",
-       edge.arrow.width=0.01, edge.arrow.size = 0.01, rescale=FALSE, add=TRUE) # plot igraph
-    plot(df_sp, add=TRUE, col = "blue") # Plot ice island polygons 
-    plot <- plot(coast, add=TRUE, col="beige")
-  
+
   return(plot)
 }
