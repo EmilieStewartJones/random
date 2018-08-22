1fe5da5a93c2fe0c40bb90494d9e4fcdec7dc2be
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-08-22 12:39:14 -0500

    Now has option of fine scale or low resolution map

--- f.coast.R
+++ f.coast.R
@@ -6,7 +6,7 @@
 #######################################################################################################
 # TO DO: - make compatable with any of the gshhs maps
 
-f.coast <- function(conn, map_extent = NULL) {
+f.coast <- function(conn, map_extent = NULL, coastm = "fine") {
   library(sp)
   
   # Define functions
@@ -17,21 +17,31 @@ f.coast <- function(conn, map_extent = NULL) {
     }
     
   # Get from db
-    query <- "SELECT ST_AsText(gshhs_l_l1.wkb_geometry) AS geom FROM gshhs_l_l1"
+    if (coastm == "fine") {
+      query <- "SELECT ST_AsText(gshhs_f_l1_subset.geom) as geom FROM gshhs_f_l1_subset"
+    }
+    else if (coastm == "low") query <- "SELECT ST_AsText(gshhs_l_l1.wkb_geometry) AS geom FROM gshhs_l_l1"
+    
     coast <- dbGetQuery(conn, query)
     row.names(coast) -> coast$gid
   
-  # Make into SpatialPolygonDataFrame
+  # Make into SpatialPolygonDataFrame (Most time consuming part of function)
+    print("----- Making into SpatialPolygonDataFrame")
     coast <- lapply(seq(nrow(coast)), FUN=function(x) readWKT(coast$geom[x], coast$gid[x]))
     coast <- do.call(rbind, coast)
+    
     # assining original proj (WGS84)
-    proj4string(coast) = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
+      print("----- Assigning original pojection")
+      proj4string(coast) = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
+    
     # reassign LCC
-    coast <- spTransform(coast, CRS("+proj=lcc +lat_1=77 +lat_2=49 +lat_0=40 +lon_0=-100, ellps=WGS84"))
+      print("----- Reassigning projection")
+      coast <- spTransform(coast, CRS("+proj=lcc +lat_1=77 +lat_2=49 +lat_0=40 +lon_0=-100, ellps=WGS84"))
   
   # crop map
     if (is.null(map_extent)) crop <- extent(0, 2000000, 2800000, 5000000)
     else crop <- extent(map_extent[1], map_extent[2], map_extent[3], map_extent[4])
+    print("----- Clipping coastline map to map extent")
     coastcrop <- gClip(coast, crop)
     
     return(coastcrop)
