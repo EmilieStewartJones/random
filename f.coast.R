b6863574e7be245644dc8d9e36f07f7ec24049b7
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-08-22 14:55:17 -0500

    Now only brings in coastline data and does not crop it

--- f.coast.R
+++ f.coast.R
@@ -1,48 +1,38 @@
 ############### Function to bring in and prepare coastline data #######################################
-#### Can only be used with gshhs_l_l1 dataframe
+#### Can be used with gshhs_l_l1 or gshhs_f_l1_subset dataframes
 ## Input: - Connection to database
-##        - Map extent as a list of coordinates, in this order: min x, max x, min y, max y (optional)
+##        - coastm: map to be used for coastline. 
+##            Options are: - fine: fine scaled map (gshhs_f_l1_subset)
+##                         - low: low resolution map (gshhs_l_l1)
 ## Output: - An object of class SpatialPolygons of the cropped coastal map
 #######################################################################################################
-# TO DO: - make compatable with any of the gshhs maps
+# TO DO: - make compatable with more of the gshhs maps
 
-f.coast <- function(conn, map_extent = NULL, coastm = "fine") {
+
+f.coast <- function(conn, coastm = "fine") {
   library(sp)
   
-  # Define functions
-    gClip <- function(shp, bb){
-      if(class(bb) == "matrix") b_poly <- as(extent(as.vector(t(bb))), "SpatialPolygons")
-      else b_poly <- as(extent(bb), "SpatialPolygons")
-      gIntersection(shp, b_poly, byid = TRUE)
-    }
-    
   # Get from db
     if (coastm == "fine") {
-      query <- "SELECT ST_AsText(gshhs_f_l1_subset.geom) as geom FROM gshhs_f_l1_subset"
+      query <- "SELECT ST_AsText(gshhs_f_l1_subset.geom) AS geom1 FROM gshhs_f_l1_subset"
     }
-    else if (coastm == "low") query <- "SELECT ST_AsText(gshhs_l_l1.wkb_geometry) AS geom FROM gshhs_l_l1"
+    else if (coastm == "low") query <- "SELECT ST_AsText(gshhs_l_l1.wkb_geometry) AS geom1 FROM gshhs_l_l1"
     
     coast <- dbGetQuery(conn, query)
     row.names(coast) -> coast$gid
   
   # Make into SpatialPolygonDataFrame (Most time consuming part of function)
-    print("----- Making into SpatialPolygonDataFrame")
-    coast <- lapply(seq(nrow(coast)), FUN=function(x) readWKT(coast$geom[x], coast$gid[x]))
+    print("-----> Making into SpatialPolygonDataFrame")
+    coast <- lapply(seq(nrow(coast)), FUN=function(x) readWKT(coast$geom1[x], coast$gid[x]))
     coast <- do.call(rbind, coast)
-    
-    # assining original proj (WGS84)
-      print("----- Assigning original pojection")
-      proj4string(coast) = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
-    
-    # reassign LCC
-      print("----- Reassigning projection")
-      coast <- spTransform(coast, CRS("+proj=lcc +lat_1=77 +lat_2=49 +lat_0=40 +lon_0=-100, ellps=WGS84"))
   
-  # crop map
-    if (is.null(map_extent)) crop <- extent(0, 2000000, 2800000, 5000000)
-    else crop <- extent(map_extent[1], map_extent[2], map_extent[3], map_extent[4])
-    print("----- Clipping coastline map to map extent")
-    coastcrop <- gClip(coast, crop)
+  # Assining original proj (WGS84)
+    print("-----> Assigning original pojection")
+    proj4string(coast) = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
     
-    return(coastcrop)
+  # Reassign LCC
+    print("-----> Reassigning projection")
+    coast <- spTransform(coast, CRS("+proj=lcc +lat_1=77 +lat_2=49 +lat_0=40 +lon_0=-100, ellps=WGS84"))
+    
+  return(coast)
 }
