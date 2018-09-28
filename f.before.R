054cd586c8e02249461e2a002cbca2157073f479
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-09-28 15:23:07 -0500

    Subsets igraph to all instances before an instance

---
+++ f.before.R
@@ -0,0 +1,13 @@
+#### To get all instances before an instance ##############################
+## Input: - g: object of class igraph 
+##        - inst: starting instance (vertex) 
+## Output: Object of class igraph 
+##########################################################################
+
+
+f.before <- function(g, inst) {
+  library(igraph)
+  v_before <- subcomponent(g, inst, mode = "in")
+  g_before <- induced_subgraph(g, v_before)
+  return(g_before)
+}
