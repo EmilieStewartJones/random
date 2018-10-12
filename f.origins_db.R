1dbd10d9134f156880b10371202d1ccffd143741
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-10-12 15:27:13 -0400

    Now creates the new columns that f.subset does

--- f.origins_db.R
+++ f.origins_db.R
@@ -15,14 +15,20 @@ f.origins_db <- function(con, calvingyr = NULL, calvingloc = NULL, orphans = TRU
      
     # Prepare full query  
     if (orphans == FALSE){
-      query <- paste0("SELECT * FROM shapefiles_qc_2013 WHERE motherinst", 
+      query <- paste0("SELECT *, ST_AsText(shapefiles_qc_2013.wkb_geometry) as geom1,", 
+                      " DENSE_RANK() OVER(PARTITION BY calvingyr ORDER BY date_trunc('week', shapefiles_qc_2013.scenedate::timestamp))", 
+                      " as wk_num FROM shapefiles_qc_2013",
+                      " WHERE motherinst", 
                       " SIMILAR TO '%(P|S)(0|1|2)(0|1|2|3|4|5|6|7|8|9)' ",
                       " AND motherinst NOT LIKE 'YYYYMMDD!_HHMMSS!_SN!_#!____' ESCAPE '!'",
                       sub_query,  
                       " OR motherinst IS NULL ", sub_query)
     }   
     else if (orphans == TRUE) {
-      query <- paste0("SELECT * FROM shapefiles_qc_2013 WHERE motherinst", 
+      query <- paste0("SELECT *, ST_AsText(shapefiles_qc_2013.wkb_geometry) as geom1,", 
+                      " DENSE_RANK() OVER(PARTITION BY calvingyr ORDER BY date_trunc('week', shapefiles_qc_2013.scenedate::timestamp))", 
+                      " as wk_num FROM shapefiles_qc_2013",
+                      " WHERE motherinst",  
                       " SIMILAR TO '%(P|S)(0|1|2)(0|1|2|3|4|5|6|7|8|9)' ",
                       "OR motherinst LIKE 'YYYYMMDD!_HHMMSS!_SN!_#!____' ESCAPE '!'",
                       sub_query,  
