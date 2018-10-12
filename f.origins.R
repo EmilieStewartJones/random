b8b81589c90c404740288b54d39fcaf341e5c0be
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-10-12 15:20:29 -0400

    Now have with or without orphan option
    Except for igraph objects (orphans will never be taken)

--- f.origins.R
+++ f.origins.R
@@ -1,25 +1,32 @@
 ###################### Returns orginal ice islands ##########################################################################
 ## Input: Object of class 'dataframe' or 'igraph'
+##             - If dataframe, you can specify if origins are included or not
+##             - If igraph, it does not include orphans   
 ## Ouput: - For igraph: a character list of the origins
 ##        - For dataframe: a dataframe subsetted to the origins
 #############################################################################################################################
-# Includes orphans and instances with motherinst like _P08, _P12, _P13, _S12...
 
 #### From Igraph or table ###########
-f.origins <- function(object) {
+f.origins <- function(object, orphans = TRUE) {
   require(igraph)
   
   # For object of class dataframe
     if (class(object) == "data.frame") {
-      orig <- subset(object, (is.na(object$motherinst) | (object$motherinst %in% grep("_P\\d|_S\\d", object$motherinst, value = TRUE))| 
-                                (object$motherinst %in% grep("YYYYMMDD_HHMMSS_SN_#_...", object$motherinst, value = TRUE))))
+      if (orphans == TRUE){
+        orig <- subset(object, (is.na(object$motherinst) | (object$motherinst %in% grep("_P\\d|_S\\d", object$motherinst, value = TRUE))| 
+                                  (object$motherinst %in% grep("YYYYMMDD_HHMMSS_SN_#_...", object$motherinst, value = TRUE))))
+      }
+      if (orphans == FALSE){
+        orig <- subset(object, (is.na(object$motherinst) | (object$motherinst %in% grep("_P\\d|_S\\d", object$motherinst, value = TRUE)) 
+                                 & !(object$motherinst %in% grep("YYYYMMDD_HHMMSS_SN_#_...", object$motherinst, value = TRUE))))
+      }
     }
     
   # For object of class igraph
     else if (class(object) == "igraph") {
-    # Make list of original instances
+    # Make list of original instances (does not include orphans)
       orig <- c(names(which(sapply(sapply(V(object), function(x) neighbors(object,x,mode='in')), length) == 0)))
-    # Removes undescribed mothers (Original instances and mothers of orphans).Do I keep this?
+      # Removes undescribed mothers (Original instances and mothers of orphans).Do I keep this?
       orig <- subset(orig, !(orig %in% grep("YYYYMMDD_HHMMSS_SN_#_|_P\\d|_S\\d", orig, value = TRUE)))
   }
   
--- f.origins_db.R
+++ f.origins_db.R
@@ -3,23 +3,31 @@
 ##        - calvingyr: A single or multiple years during which the instance originated. If none are specified, all will be used
 ##        - calvingloc: A single or multiple orginal calving locations. If none are specified, all will be used
 ################################################################################################################################
-# Includes orphans and instances with motherinst like _P08, _P12, _P13, _S12...
 
 ###### From database ######
-f.origins_db <- function(con, calvingyr = NULL, calvingloc = NULL) {
+f.origins_db <- function(con, calvingyr = NULL, calvingloc = NULL, orphans = TRUE) {
   require(RPostgreSQL)
   source('f.subquery.R')
   
   # Prepare DB query
     # Form subquery
       sub_query <- f.subquery(calvingyr, calvingloc)
-
-    # Prepare full query
+     
+    # Prepare full query  
+    if (orphans == FALSE){
       query <- paste0("SELECT * FROM shapefiles_qc_2013 WHERE motherinst", 
                       " SIMILAR TO '%(P|S)(0|1|2)(0|1|2|3|4|5|6|7|8|9)' ",
+                      " AND motherinst NOT LIKE 'YYYYMMDD!_HHMMSS!_SN!_#!____' ESCAPE '!'",
                       sub_query,  
-                      # " motherinst NOT LIKE 'YYYYMMDD!_HHMMSS!_SN!_#!____' ESCAPE '!'AND ",
                       " OR motherinst IS NULL ", sub_query)
+    }   
+    else if (orphans == TRUE) {
+      query <- paste0("SELECT * FROM shapefiles_qc_2013 WHERE motherinst", 
+                      " SIMILAR TO '%(P|S)(0|1|2)(0|1|2|3|4|5|6|7|8|9)' ",
+                      "OR motherinst LIKE 'YYYYMMDD!_HHMMSS!_SN!_#!____' ESCAPE '!'",
+                      sub_query,  
+                      " OR motherinst IS NULL ", sub_query)
+    }  
     
   # Query db
     orig <- dbGetQuery(con, query)
