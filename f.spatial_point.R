0e7ee339077ba49408b79ed157055996f6c8ee2a
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-12-17 17:20:41 -0500

    Slight function name change

--- f.spatial_point.R
+++ f.spatial_point.R
@@ -1,6 +1,6 @@
 #################### Create spatial points dataframe ################################
 
-f.Spatial_pdf <- function(df) {
+f.spatial_point <- function(df) {
   coordinates(df) <- ~centroid_x+centroid_y
   # Assining original proj (LCC)
   proj4string(df) = CRS("+proj=lcc +lat_1=77 +lat_2=49 +lat_0=40 +lon_0=-100, ellps=WGS84")
