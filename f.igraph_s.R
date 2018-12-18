ba129e3a37c3acfa4fd82c67ea07df15dcaa227f
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-12-18 11:51:45 -0500

    More small changes

--- f.igraph_s.R
+++ f.igraph_s.R
@@ -1,11 +1,5 @@
 #################### Function to create spatially refrenced igraph object #####################
-## Input: - Dataframe queried from database with inst, motherinst and geom1 columns 
-## Output:   - spatially referenced igraph object
-##           - Spatial polygons dataframe
-###############################################################################################
-# TO DO: Find a way to return both the igraph and the spatial polygons dataframe
-#              - Put spatial polygons dataframe into igraph object??
-# Creating the spatial points shapefile of centroids is redundant because that info already exists in the dataframe
+
 
 f.igraph_s <- function(df) {
   require(igraph)
@@ -28,11 +22,11 @@ f.igraph_s <- function(df) {
     names(undescribedmothers)[1] <- 'inst'
     vattrib_df <- rbind.fill(vattrib_df,undescribedmothers)  # Now the undescribed mothers are in the vertices attribute table without any attributes (because they don't have an inst)
   
-  # Make spatial polygons dataframe 
-    df_spoint <- f.spatial_point(df)
-  
   # Creating object of class igraph
     g <- graph_from_data_frame(edgelist, vertices = vattrib_df) #igraph function to make a graph using edges and assign attribs to vertices
+  
+  # Make spatial points dataframe 
+    df_spoint <- f.spatial_point(df)
   
   # Add spatial points df to igraph
     V(g)$x=df_spoint@data$centroid_x[match(V(g)$name, df_spoint@data$inst)]
