d48415d53aa641d1096e4d6515996a28c625485a
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-12-18 11:37:23 -0500

    Made more efficient
    Was making spatial polygons dataframe and then converting into spatial points dataframe instead of creating a spatial points dataframe directly.

--- f.igraph_s.R
+++ f.igraph_s.R
@@ -5,40 +5,42 @@
 ###############################################################################################
 # TO DO: Find a way to return both the igraph and the spatial polygons dataframe
 #              - Put spatial polygons dataframe into igraph object??
+# Creating the spatial points shapefile of centroids is redundant because that info already exists in the dataframe
 
 f.igraph_s <- function(df) {
   require(igraph)
   require(sp)
   require(rgeos)
   require(plyr)
-  source('f.Spatialdf.R')
+  source('f.spatial_point.R')
   
   # Make spatial polygons dataframe 
-   df_sp <<- f.Spatialdf(df)
-   
+  df_spoint <- f.spatial_point(df)
+  
   # Make edgelist 
-    df_g <- df_sp@data
-    edgelist <- data.frame(df_g$motherinst, df_g$inst) 
-    colnames(edgelist) <- c("motherinst", "inst")
-    edgelist <- as.data.frame(edgelist[edgelist$motherinst %in% edgelist$inst,])
+  df_g <- df_spoint@data
+  #df_g <-df
+  
+  edgelist <- data.frame(df_g$motherinst, df_g$inst) 
+  colnames(edgelist) <- c("motherinst", "inst")
+  edgelist <- as.data.frame(edgelist[edgelist$motherinst %in% edgelist$inst,])
   
   # Creating attribute table for vertices
-    undescribedmothers <- na.omit(data.frame(unique(df_g$motherinst[!(df_g$motherinst %in% df_g$inst)]),
-                                          stringsAsFactors = F))  # these are the mothers that are referred to in the edgelist but have no inst
-    vattrib_df <- cbind(df_g$inst, df_g[,names(df_g)!='inst'], stringsAsFactors =F)  # make a dataframe that starts with the vertex name and matches the inst in edgelist
-    names(vattrib_df)[1] <- 'inst'
-    names(undescribedmothers)[1] <- 'inst'
-    vattrib_df <- rbind.fill(vattrib_df,undescribedmothers)  # Now the undescribed mothers are in the vertices attribute table without any attributes (because they don't have an inst)
+  undescribedmothers <- na.omit(data.frame(unique(df_g$motherinst[!(df_g$motherinst %in% df_g$inst)]),
+                                           stringsAsFactors = F))  # these are the mothers that are referred to in the edgelist but have no inst
+  vattrib_df <- cbind(df_g$inst, df_g[,names(df_g)!='inst'], stringsAsFactors =F)  # make a dataframe that starts with the vertex name and matches the inst in edgelist
+  names(vattrib_df)[1] <- 'inst'
+  names(undescribedmothers)[1] <- 'inst'
+  vattrib_df <- rbind.fill(vattrib_df,undescribedmothers)  # Now the undescribed mothers are in the vertices attribute table without any attributes (because they don't have an inst)
   
-  # Make a point shapefile of ice island centroids
-    points_c <- lapply(seq(length(df_sp)), FUN=function(x) gCentroid(df_sp[x,]))
-    points_c <- do.call(rbind, points_c)
-    df_sh <- data.frame(df_sp@data,coordinates(points_c))
-   
+  
   # Creating object of class igraph
-    g <- graph_from_data_frame(edgelist, vertices = vattrib_df) #igraph function to make a graph using edges and assign attribs to vertices
-    V(g)$x=df_sh$x[match(V(g)$name, df_sh$inst)]
-    V(g)$y=df_sh$y[match(V(g)$name, df_sh$inst)]
+  g <- graph_from_data_frame(edgelist, vertices = vattrib_df) #igraph function to make a graph using edges and assign attribs to vertices
+  V(g)$x=df_spoint@data$centroid_x[match(V(g)$name, df_spoint@data$inst)]
+  V(g)$y=df_spoint@data$centroid_y[match(V(g)$name, df_spoint@data$inst)]
   
   return(g)
 }
+
+
+
