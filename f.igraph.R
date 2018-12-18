120e0058bbd3083d8b07144586676f4f5817c4ee
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-12-18 11:53:47 -0500

    Combines f.igraph and f.igraph_s
    Can now be used to create a spatially referenced igraph or not. Just need to specify if spatial is TRUE or FALSE

--- f.igraph.R
+++ f.igraph.R
@@ -1,20 +1,18 @@
 ######################## Function that creates an igraph object ##############################
-##### Input:  Dataframe with at the least the motherinst and inst columns
-##### Output: igraph object that includes all attributes ####
-##############################################################################################
 
-f.igraph <- function(table){
+
+f.igraph <- function(df, spatial = TRUE){
   library(igraph)
   
   # Create edgelist 
-    edgelist <- data.frame(table$motherinst, table$inst) 
+    edgelist <- data.frame(df$motherinst, df$inst) 
     colnames(edgelist) <- c("motherinst", "inst")
     edgelist <- as.data.frame(edgelist[edgelist$motherinst %in% edgelist$inst,])
   
   # Creating attribute table for vertices
-    undescribedmothers = na.omit(data.frame(unique(table$motherinst[!(table$motherinst %in% table$inst)]),
+    undescribedmothers = na.omit(data.frame(unique(df$motherinst[!(df$motherinst %in% df$inst)]),
                                           stringsAsFactors = F))  # these are the mothers that are referred to in the edgelist but have no inst
-    vattrib_df = cbind(table$inst, table[,names(table)!='inst'], stringsAsFactors =F)  # make a dataframe that starts with the vertex name and matches the inst in edgelist
+    vattrib_df = cbind(df$inst, df[,names(df)!='inst'], stringsAsFactors =F)  # make a dataframe that starts with the vertex name and matches the inst in edgelist
     names(vattrib_df)[1] = 'inst'
     names(undescribedmothers)[1] = 'inst'
     vattrib_df = rbind.fill(vattrib_df,undescribedmothers)  # Now the undescribed mothers are in the vertices attribute table without any attributes (because they don't have an inst)
@@ -22,5 +20,12 @@ f.igraph <- function(table){
   # Creating object of class igraph
     g <- graph_from_data_frame(edgelist, vertices = vattrib_df) #igraph function to make a graph using edges and assign attribs to vertices
   
+    if (spatial == TRUE){
+      # Make spatial points dataframe 
+      df_spoint <- f.spatial_point(df)
+      # Add spatial points df to igraph
+      V(g)$x=df_spoint@data$centroid_x[match(V(g)$name, df_spoint@data$inst)]
+      V(g)$y=df_spoint@data$centroid_y[match(V(g)$name, df_spoint@data$inst)]
+    }
   return(g)
 }
