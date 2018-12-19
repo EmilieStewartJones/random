90d3f6597a645ff38281efb00fd2fa355daeddd4
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-12-19 17:47:25 -0500

    Combines f.fract_bf and f.fract_af

--- f.fract.R
+++ f.fract.R
@@ -1,54 +1,15 @@
-##### Combine the 2 #####
-# fract_bf + fract_af does not equal fract because of overlap.
+# Just before fracturing from dataframe #
 
-f.fract <- function(con, type = "all", calvingyr = NULL, calvingloc = NULL, wk_num = NULL) {
-  require(RPostgreSQL)
-  #source('f.subquery.R')
-  
-  # Form subquery
-    sub_query <- f.subquery(calvingyr, calvingloc)
-  
-  if (type == "all"){
-    # Query db for all instances just after fracturing  
-    query <- paste0("SELECT *, ST_AsText(shapefiles_qc_2013.wkb_geometry) as geom1,", 
-                    " DENSE_RANK() OVER(PARTITION BY calvingyr ORDER BY date_trunc('week', shapefiles_qc_2013.scenedate::timestamp))", 
-                    " as wk_num FROM shapefiles_qc_2013",
-                    " WHERE motherinst IN (SELECT motherinst FROM shapefiles_qc_2013 WHERE",
-                    " motherinst NOT LIKE 'YYYYMMDD!_HHMMSS!_SN!_#!____' ESCAPE '!'",
-                    " AND motherinst NOT SIMILAR TO '%(P|S)(0|1|2)(0|1|2|3|4|5|6|7|8|9)'",
-                    sub_query,
-                    " GROUP BY motherinst HAVING COUNT(*) > 1)",
-                    " OR inst IN (SELECT motherinst FROM shapefiles_qc_2013 ",
-                    " GROUP BY motherinst HAVING COUNT(*) > 1)",
-                    sub_query)
+f.fract_bf <- function(object, type="before") {
+  if (type = "before"){
+    # Before fracture
+    fract <- subset(object, object$inst %in% count(object$motherinst)$x[which(count(object$motherinst)$freq >= 2)])
   }
-    else if (type =="before") {
-      # Query db for all instances just before fracturing  
-      query <- paste0("SELECT *, ST_AsText(shapefiles_qc_2013.wkb_geometry) as geom1,", 
-                      " DENSE_RANK() OVER(PARTITION BY calvingyr ORDER BY date_trunc('week', shapefiles_qc_2013.scenedate::timestamp))", 
-                      " as wk_num FROM shapefiles_qc_2013",
-                      " WHERE inst IN (SELECT motherinst FROM shapefiles_qc_2013 ",
-                      " GROUP BY motherinst HAVING COUNT(*) > 1)", sub_query)
-    }
-    else if (type == "after") {
-      # Query db for all instances just after fracturing  
-      query <- paste0("SELECT *, ST_AsText(shapefiles_qc_2013.wkb_geometry) as geom1,", 
-                      " DENSE_RANK() OVER(PARTITION BY calvingyr ORDER BY date_trunc('week', shapefiles_qc_2013.scenedate::timestamp))", 
-                      " as wk_num FROM shapefiles_qc_2013",
-                      " WHERE motherinst IN (SELECT motherinst FROM shapefiles_qc_2013 WHERE ",
-                      " motherinst NOT LIKE 'YYYYMMDD!_HHMMSS!_SN!_#!____' ESCAPE '!'",
-                      " AND motherinst NOT SIMILAR TO '%(P|S)(0|1|2)(0|1|2|3|4|5|6|7|8|9)'",
-                      " GROUP BY motherinst HAVING COUNT(*) > 1)", sub_query)
-    }
-
-    fract <- dbGetQuery(con, query)
-    
-  # Subset weeks of interest from dataframe
-    if (is.null(wk_num)){
-      fract <- fract
-    }
-    else fract <- fract[fract$wk_num >= wk_num[1] & fract$wk_num <= wk_num[2], ]
-  
+  if (type = "after"){
+    # After fracture
+    fract <- subset(object, object$motherinst %in% count(object$motherinst)$x[which(count(object$motherinst)$freq >= 2)] &
+                         !(object$motherinst %in% grep("_P\\d|_S\\d", object$motherinst, value = TRUE)) &
+                         !(object$motherinst %in% grep("YYYYMMDD_HHMMSS_SN_#_...", object$motherinst, value = TRUE)))
+  }
   return(fract)
 }
-
