e7053ee55ad4c75c19f43f674c3f1740d796cd03
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-12-20 21:36:55 -0500

    Minor changes

---
+++ f.plot_df.R
@@ -0,0 +1,63 @@
+############################# Plotting of dataframe ###############################
+f.plot_df <- function(object, coastline = NULL, title = "", col = "blue", legend=FALSE, title_leg = ""){
+  par(mar=c(1,1,1.5,1))
+  ############################ Functions ##############################
+  ### Plotting spatial points
+  plot_pnt <- function(object, add, legend){
+    plot(object, col= col, pch = 20, add=add, main=title)
+    if (legend == TRUE){
+      legend("bottomleft", legend=unique(col), col= unique(col), pch = 16, title=title_leg)
+    }
+    else if (legend == FALSE) {}
+  }
+  
+  ### Plotting spatial polygons
+  plot_pol <- function(object, add, legend){
+    plot(object, col = col, border= "black", add=add, main=title)
+    if (legend == TRUE){
+      legend("topright", legend=unique(col), col= unique(col), pch = 16, title=title_leg)
+    }
+    else if (legend == FALSE) {}
+  }
+  ############################ Coastline ##############################
+  if (!is.null(coastline)){
+    print("Preparing coastline data")
+    
+    # crop coastline map
+    map_extent <- c(min(object@data$centroid_x) - 25000, max(object@data$centroid_x) + 25000,
+                    min(object@data$centroid_y) - 25000, max(object@data$centroid_y) + 25000)
+    print("-----> Clipping coastline map to map extent")
+    if (is.null(map_extent)) crop <- extent(0, 2000000, 2800000, 5000000)
+    else crop <- extent(map_extent[1], map_extent[2], map_extent[3], map_extent[4])
+    
+    coast <- gClip(coastline, crop)
+  }
+  ############################ Type #######################################
+  if (!is.null(coastline)){
+    if (class(object) == "SpatialPolygonsDataFrame"){
+      ## PLOT ##
+      print("Plotting")
+      plot(coast, col="beige") # plot coastline
+      plot<- plot_pol(object, add=TRUE, legend)
+      #plot <- plot(coast, add=TRUE, col="beige")
+    }
+    else if (class(object)== "SpatialPointsDataFrame"){
+      print("Plotting")
+      plot(coast, col = "beige", main = title) # plot coastline
+      plot <- plot_pnt(object, add=TRUE, legend) 
+    }
+  }
+  else {
+    if (class(object) == "SpatialPolygonsDataFrame"){
+      ## PLOT ##
+      print("Plotting")
+      plot <- plot_pol(object, add=FALSE, legend)
+    }
+    else if (class(object)== "SpatialPointsDataFrame"){
+      print("Plotting")
+      plot <- plot_pnt(object, add=FALSE, legend) 
+    }
+  }
+}
+
+# as.factor(object$calvingyr)
---
+++ f.plot_igraph.R
@@ -0,0 +1,93 @@
+####################### THE plotting function ################################
+# Grid and tree require igraph and not igraph_s
+# Tree does not work
+# Could use igraph for everything and remove igraph_s
+
+f.plot_igraph <- function(g, type, coastline = NULL, col = "blue", size = 1, coords, title = ""){
+  ############################ Functions ##############################
+  ### Plotting spatial igraph
+  plot_g <- function(g){
+    plot(g, add=TRUE, vertex.shape="none", vertex.label="",
+         edge.arrow.width=1, edge.arrow.size = 0.3, rescale=FALSE, 
+         vertex.label = V(g)$inst, vertex.label.cex = 1, vertex.label.color = 'black')
+  }
+  ### Plotting spatial points
+  plot_pnt <- function(coords, add, size){
+    plot(coords, col= col, pch = 21, add=add, cex=size, main=title)
+  }
+  
+  ### Plotting spatial polygons
+  plot_pol <- function(coords, add){
+    plot(coords, col = "blue", border= "black", add=add, main = title)
+  }
+  
+  ############################ Coastline ##############################
+if (!is.null(coastline)){
+  print("Preparing coastline data")
+  
+  # crop coastline map
+  map_extent <- c(min(coords@data$centroid_x) - 25000, max(coords@data$centroid_x) + 25000,
+                  min(coords@data$centroid_y) - 25000, max(coords@data$centroid_y) + 25000)
+  print("-----> Clipping coastline map to map extent")
+  if (is.null(map_extent)) crop <- extent(0, 2000000, 2800000, 5000000)
+  else crop <- extent(map_extent[1], map_extent[2], map_extent[3], map_extent[4])
+  
+  coast <- gClip(coastline, crop)
+}
+  
+  ############################ Type ###################################  
+  if (type == "tree"){
+    print("Plotting")
+    plot(g, edge.arrow.size=0.3, vertex.frame.color = "white", 
+         vertex.color=rbPal(10)[as.numeric(cut(sqrt(V(g)$poly_area),breaks = 10))], 
+         vertex.size= degree(g)*2,
+         #vertex.color=rbPal(10)[as.numeric(cut(degree(g)*2,breaks = 10))], vertex.size= sqrt(V(g)$poly_area)*1.5, 
+         edge.curved = 0.1, vertex.label.cex = 1, vertex.label=NA, layout = layout_as_tree, main=title)
+  }
+  else if (type == "grid"){
+    print("Plotting")
+    plot(g, layout_on_grid(g, width = 0, height = 0, dim = 2), edge.arrow.size=0.3, vertex.frame.color = "white", 
+         vertex.color=rbPal(10)[as.numeric(cut(sqrt(V(g)$poly_area),breaks = 10))], 
+         vertex.size= degree(g)*2,
+         #vertex.color=rbPal(10)[as.numeric(cut(degree(g)*2,breaks = 10))], vertex.size= sqrt(V(g)$poly_area)*1.5, 
+         edge.curved = 0.1, vertex.label=NA, vertex.label.cex = 1, main = title)
+  }
+  else if (type == "spatial"){
+    if (!is.null(coastline)){
+      
+      if (class(coords) == "SpatialPolygonsDataFrame"){
+        ## PLOT ##
+        print("Plotting")
+        par(mar=c(1,1,1,1))
+        plot(coast) # plot coastline
+        plot_pol(coords, add=TRUE)
+        plot_g(g)
+        plot <- plot(coast, add=TRUE, col="beige")
+      }
+      else if (class(coords)== "SpatialPointsDataFrame"){
+        print("Plotting")
+        par(mar=c(1,1,1,1))
+        plot(coast) # plot coastline
+        plot_pnt(coords, add=TRUE, size) 
+        plot_g(g)
+        #legend("bottomleft", legend=as.factor(unique(object$calvingyr)), col= as.factor(unique(object$calvingyr)), pch = 19, title="Year")
+        plot <- plot(coast, add=TRUE, col="beige")
+      }
+    }
+    else {
+      if (class(coords) == "SpatialPolygonsDataFrame"){
+        ## PLOT ##
+        print("Plotting")
+        par(mar=c(1,1,1,1))
+        plot_pol(coords, add=FALSE)
+        plot <- plot_g(g)
+      }
+      else if (class(coords)== "SpatialPointsDataFrame"){
+        print("Plotting")
+        plot_pnt(coords, add=FALSE, size) 
+        plot <- plot_g(g)
+        #legend("bottomleft", legend=as.factor(unique(object$calvingyr)), col= as.factor(unique(object$calvingyr)), pch = 19, title="Year")
+      }
+    }
+  }
+}
