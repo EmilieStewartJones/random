6e68804f83b932bd332cf34673d4d6bd7a203710
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2019-03-08 12:10:27 -0500

    Delete f.fract_bf.R
    No longer needed in Version 2.0 because f.fract_db includes both before and after options.

--- f.fract_bf.R
+++
@@ -1,8 +0,0 @@
-# Just before fracturing from dataframe #
-
-f.fract_bf <- function(object) {
-  # From table
-  # Before fracture
-  fract_bf <- subset(object, object$inst %in% count(object$motherinst)$x[which(count(object$motherinst)$freq >= 2)])
-  return(fract_bf)
-}
